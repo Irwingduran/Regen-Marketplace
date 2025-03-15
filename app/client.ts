947caf0fd3f04e3a69141c079ef3c03d8f6e4e3a
Irwingduran <86335744+Irwingduran@users.noreply.github.com>
2025-03-15 16:46:24 -0600

    Delete app directory

--- app/client.ts
+++
@@ -1,6 +0,0 @@
-import { createThirdwebClient } from "thirdweb";
-const clientId =  process.env.THIRDWEB_SECRET_KEY;
-
-export const client = createThirdwebClient({
-  clientId: clientId as string,
-});
